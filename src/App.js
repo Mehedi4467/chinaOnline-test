1b43d151c41b707e84a77d01a765c5cf28a86fa3
Mehedi Hassan <mehedihassan4467@gmail.com>
2022-12-05 11:36:31 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 function App() {
   return (
     <div>
-      <p>Hello Messanger</p>
+      <p>Hello Messenger</p>
     <MessengerCustomerChat
     pageId="2535038766546321"
     appId="1485882018564385"
